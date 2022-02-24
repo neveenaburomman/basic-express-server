c5da6ef8bbecd2f413c33cbcec4d762cbed40d12
Neveen97 <neveenaburomman1@gmail.com>
2022-02-24 13:42:06 +0200

    second try

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -1,12 +1,11 @@
 
 function validator(req,res,next){
  // console.log(typeof(req.query));
-const check = /^[0-9]+$/;
-const result =(req.query.name).match(check)
+//const check = /^[0-9]+$/;
+//const result =(req.query.name).match(check)
 //console.log(result);
 
-if ( (req.query.name) !== "" && !result)
-
+if ( (req.query.name) !== "" && !isNaN(req.query.name))
 {
 next()
 }
